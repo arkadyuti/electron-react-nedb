faa402d37cc414f1b2238630c9b3ec18886ecdf0
SAPIENT\skund2 <skundu@sapient.com>
2017-07-10 00:55:07 +0530

    feat: added dashboard

--- app/components/DashBoard.jsx
+++ app/components/DashBoard.jsx
@@ -1,16 +1,46 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 export default class DashBoard extends Component {
 	constructor(props) {
 		super(props);
 	}
 
+	_handleBillingIconClick = () => {
+		this.props.history.push('/billing');
+	}
+
+	_handleOldBillingIconClick = () => {
+		this.props.history.push('/oldBill');
+	}
+
+	_handleCartIconClick = () => {
+		this.props.history.push('/stock');
+	}
+
+	_handleAddBillingIconClick = () => {
+		this.props.history.push('/entry');
+	}
+
 	render() {
 		return (
-		  <div className="pane padded-bottom-more">
-        A DashBoard
+		  <div className="pane padded-bottom-more flex-space-around">
+				<div onClick={this._handleBillingIconClick} className="cursor">
+					<div className="billing-icon"></div>
+					<div className="text-center">Billing</div>
+				</div>
+				<div onClick={this._handleOldBillingIconClick} className="cursor">
+					<div className="old-bill-icon"></div>
+					<div className="text-center">View Old Bill</div>
+				</div>
+				<div onClick={this._handleCartIconClick} className="cursor">
+					<div className="cart-icon"></div>
+					<div className="text-center">My Stock</div>
+				</div>
+				<div onClick={this._handleAddBillingIconClick} className="cursor">
+					<div className="add-bill-icon"></div>
+					<div className="text-center">Add Entry</div>
+				</div>
       </div>
 		);
 	}
