3444191d2719fe8ebbfcdecfeb0191f97cf494cd
arkadyuti <arkadooti.sarkar@gmail.com>
2017-09-13 13:47:05 +0530

    Notification and minor changes

--- app/components/AddEntry.jsx
+++ app/components/AddEntry.jsx
@@ -11,33 +11,36 @@ export default class AddEntry extends Component {
 			notification: false
 		}
 	}
-	
+
 	_handleFormData = (e, refs) => {
 		let _this = this;
 		const { state } = this.__el;
 
 		this.props.dbStock.insert(state, function (err, data) {   // Callback is optional
 			console.log("Data Inserted", data, err);
-			_this.setState({ notification: true})
+			_this.setState({ notification: true });
+			setTimeout(_this.removeNotification, 3000);
 		});
 		document.forms[0].reset();
 	};
-
+	removeNotification = () => {
+		this.setState({ notification: false })
+	}
 	render() {
 		const productCatagoryOptions = [
-			{ label : 'one', value: 1 }, { label : 'two', value: 2 }
+			{ label: 'one', value: 1 }, { label: 'two', value: 2 }
 		]
+		console.log(this.state.notification)
 		return (
-		  <div className="pane padded-bottom-more entry-form">
+			<div className="pane padded-bottom-more entry-form">
 				<EntryForm
+					notification={this.state.notification ? "Data inserted" : ""}
 					productCatagoryOptions={productCatagoryOptions}
 					onSubmit={this._handleFormData}
 					defaultProductCatagorySelectedOption={2}
 					ref={c => { this.__el = c; return this.__el; }}
 				/>
-				{this.state.notification && <Success message="Your entry added" />}
-      </div>
+			</div>
 		);
 	}
 }
-          
--- app/components/common/EntryForm.jsx
+++ app/components/common/EntryForm.jsx
@@ -4,7 +4,7 @@ import FormInput from 'components/common/FormInput';
 class EntryForm extends Component {
   constructor(props) {
     super(props);
-    const { 
+    const {
       slNo,
       date,
       supplier,
@@ -15,7 +15,7 @@ class EntryForm extends Component {
       price,
       defaultProductCatagorySelectedOption
     } = props;
-    this.state= {
+    this.state = {
       slNo,
       date,
       supplier,
@@ -55,22 +55,25 @@ class EntryForm extends Component {
 
     if (typeof onSubmit === 'function') {
       onSubmit(e, __form);
+      this._clearForm();
     }
   };
-  _clearForm (e){
-    e.currentTarget.parentElement.parentElement.reset();
+  _clearForm(e) {
+    if (e && currentTarget) {
+      e.currentTarget.parentElement.parentElement.reset();
+    }
     this.setState({
-      slNoValue : "",
-      dateValue : "",
-      supplierValue : "",
-      productIDValue : "",
-      productNameValue : "",
-      productDescriptionValue : "",
-      quantityValue : "",
-      PriceValue : "",
-      defaultProductCatagorySelectedOption : ""
+      slNo: "",
+      date: "",
+      supplier: "",
+      productID: "",
+      productName: "",
+      productDescription: "",
+      quantity: "",
+      price: "",
+      defaultProductCatagorySelectedOption: ""
     })
-  } 
+  }
   _productCatagoryHandleChange = (e) => {
     const { value } = e.target;
     this.setState({
@@ -80,35 +83,35 @@ class EntryForm extends Component {
 
   _updateSlNo = (e) => {
     const { value } = e.target;
-    
+
     this.setState({
       slNo: value
     });
   };
   _updateDate = (e) => {
     const { value } = e.target;
-    
+
     this.setState({
       date: value
     });
   };
   _updateSupplier = (e) => {
     const { value } = e.target;
-    
+
     this.setState({
       supplier: value
     });
   };
   _updateProductID = (e) => {
     const { value } = e.target;
-    
+
     this.setState({
       productID: value
     });
   };
   _updateProductName = (e) => {
     const { value } = e.target;
-    
+
     this.setState({
       productName: value
     });
@@ -116,7 +119,7 @@ class EntryForm extends Component {
 
   _updateProductDescription = (e) => {
     const { value } = e.target;
-    
+
     this.setState({
       productDescription: value
     });
@@ -124,14 +127,14 @@ class EntryForm extends Component {
 
   _updateQuantity = (e) => {
     const { value } = e.target;
-    
+
     this.setState({
       quantity: value
     });
   };
   _updatePrice = (e) => {
     const { value } = e.target;
-    
+
     this.setState({
       price: value
     });
@@ -139,7 +142,7 @@ class EntryForm extends Component {
 
   render() {
     const { state, props } = this;
-    const { 
+    const {
       slNo,
       date,
       supplier,
@@ -151,8 +154,8 @@ class EntryForm extends Component {
       defaultProductCatagorySelectedOption
     } = state;
 
-    const { productCatagoryOptions, formRef } = props;
-    
+    const { productCatagoryOptions, formRef, notification } = props;
+
     const selectOptionOptionArr = productCatagoryOptions && productCatagoryOptions.map((item, key) => {
       const args = Object.assign({}, {
         id: `option-${key}`,
@@ -164,35 +167,35 @@ class EntryForm extends Component {
       );
     });
 
-		return(
-			<form className="" onSubmit={this._handleFormData} ref={c => { this.__form = c; return this.__form; }}>
+    return (
+      <form className="" onSubmit={this._handleFormData} ref={c => { this.__form = c; return this.__form; }}>
         <div className="form-group">
           <label>Sl No.</label>
-          <FormInput type="text" ref="slNo" value={slNo}	className="form-control" placeholder="Sl No." onChange={this._updateSlNo} />
+          <FormInput type="text" ref="slNo" value={slNo} className="form-control" placeholder="Sl No." onChange={this._updateSlNo} />
         </div>
         <div className="form-group">
           <label>Date</label>
-          <FormInput type="date"ref="date" value={date} className="form-control" placeholder="Date" onChange={this._updateDate}	/>
+          <FormInput type="date" ref="date" value={date} className="form-control" placeholder="Date" onChange={this._updateDate} />
         </div>
         <div className="form-group">
           <label>Supplier</label>
-          <FormInput type="text" value={supplier} className="form-control" placeholder="Supplier" ref="supplier" onChange={this._updateSupplier}/>
+          <FormInput type="text" value={supplier} className="form-control" placeholder="Supplier" ref="supplier" onChange={this._updateSupplier} />
         </div>
         <div className="form-group">
           <label>Product ID</label>
-          <FormInput type="text" value={productID} className="form-control" ref= "productID" placeholder="Product ID" onChange={this._updateProductID} />
+          <FormInput type="text" value={productID} className="form-control" ref="productID" placeholder="Product ID" onChange={this._updateProductID} />
         </div>
         <div className="form-group">
           <label>Product Name</label>
-          <FormInput type="text" className="form-control" value={productName} ref= "productName" placeholder="Product Name" onChange={this._updateProductName} />
+          <FormInput type="text" className="form-control" value={productName} ref="productName" placeholder="Product Name" onChange={this._updateProductName} />
         </div>
         <div className="form-group">
           <label>Product Description</label>
-          <textarea type="text" className="form-control"  value={productDescription} ref= "productDescription" placeholder="Product Description" onChange={this._updateProductDescription} />
+          <textarea type="text" className="form-control" value={productDescription} ref="productDescription" placeholder="Product Description" onChange={this._updateProductDescription} />
         </div>
         <div className="form-group">
           <label>Product Catagory</label>
-          <select className="form-control" ref= "productCatagory" value={defaultProductCatagorySelectedOption} onChange={this._productCatagoryHandleChange}>
+          <select className="form-control" ref="productCatagory" value={defaultProductCatagorySelectedOption} onChange={this._productCatagoryHandleChange}>
             {selectOptionOptionArr}
           </select>
         </div>
@@ -202,15 +205,20 @@ class EntryForm extends Component {
         </div>
         <div className="form-group">
           <label>Price</label>
-          <FormInput type="text" value={price} className="form-control"  ref="price" placeholder="Price" onChange={this._updatePrice} />
+          <FormInput type="text" value={price} className="form-control" ref="price" placeholder="Price" onChange={this._updatePrice} />
         </div>
         <div className="form-actions">
           <button type="reset" className="btn btn-form btn-default" onClick={this._clearForm}>Cancel</button>
           <button type="submit" className="btn btn-form btn-primary">OK</button>
         </div>
-      </form>
-		);
-	}
+        { notification && 
+        <div className="form-group">
+          <label style={{padding: "15px 0",fontWeight: "bold",fontSize: "15px"}}>{notification}</label>
+        </div>
+        }
+      </form >
+    );
+  }
 }
 
 EntryForm.propTypes = {
